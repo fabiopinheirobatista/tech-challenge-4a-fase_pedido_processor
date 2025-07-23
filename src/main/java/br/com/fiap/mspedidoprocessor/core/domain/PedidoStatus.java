b3851136135be925e478a6f3351810dd8d4f2fb4
Vinícius Henrique Dias de Souza <vinihds@gmail.com>
2025-07-23 10:12:31 -0300

    Implementado consumo buscarProdutoService

---
+++ src/main/java/br/com/fiap/mspedidoprocessor/core/domain/PedidoStatus.java
@@ -0,0 +1,9 @@
+package br.com.fiap.mspedidoprocessor.core.domain;
+
+public enum PedidoStatus {
+    ABERTO,
+    FALHADO,
+    FECHADO_SEM_ESTOQUE,
+    FECHADO_COM_SUCESSO,
+    FECHADO_SEM_CREDITO
+}
